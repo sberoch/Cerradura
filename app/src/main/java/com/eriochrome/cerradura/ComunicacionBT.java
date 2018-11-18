41a2d18d5113697bea4dcd3b32c2064fce03dd4d
sberoch <sberoch@gmail.com>
2018-11-18 19:18:46 -0300

    encuentra dispositivos

--- app/src/main/java/com/eriochrome/cerradura/ComunicacionBT.java
+++ app/src/main/java/com/eriochrome/cerradura/ComunicacionBT.java
@@ -1,8 +1,14 @@
 package com.eriochrome.cerradura;
 
+import android.Manifest;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +22,8 @@ import java.util.Set;
 
 public class ComunicacionBT extends AppCompatActivity {
 
+    private int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+
     private BluetoothAdapter btAdapter;
 
     private ArrayList<String> dispositivos;
@@ -23,7 +31,23 @@ public class ComunicacionBT extends AppCompatActivity {
 
     private ListView listaDispositivosView;
 
-    private Button refresh;
+    private Button refrescar;
+    private Button buscar;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Toast.makeText(ComunicacionBT.this, "Se encontro un dispositivo.", Toast.LENGTH_SHORT).show();
+                BluetoothDevice dispositivo = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String nombreDisp = dispositivo.getName();
+                String addressDisp = dispositivo.getAddress();
+                dispositivos.add(nombreDisp + "\n" + addressDisp);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 
 
     @Override
@@ -33,18 +57,33 @@ public class ComunicacionBT extends AppCompatActivity {
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        listaDispositivosView = findViewById(R.id.listview);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
 
+        listaDispositivosView = findViewById(R.id.listview);
         dispositivos = new ArrayList<>();
         adapter = new DevicesAdapter(dispositivos, this);
         listaDispositivosView.setAdapter(adapter);
 
-        refresh = findViewById(R.id.refresh);
+        refrescar = findViewById(R.id.refresh);
+        buscar = findViewById(R.id.buscar);
 
-        refresh.setOnClickListener(new View.OnClickListener() {
+        refrescar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dispositivos.clear();
                 showPairedDevices();
+            }
+        });
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btAdapter.isDiscovering()) {
+                    Toast.makeText(ComunicacionBT.this ,"Ya estabas buscando dispositivos", Toast.LENGTH_SHORT).show();
+                    btAdapter.cancelDiscovery();
+                }
+                btAdapter.startDiscovery();
+                Toast.makeText(ComunicacionBT.this, "Buscando dispositivos..", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -52,6 +91,11 @@ public class ComunicacionBT extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
         if (!btAdapter.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOn, 0);
@@ -70,5 +114,15 @@ public class ComunicacionBT extends AppCompatActivity {
             Toast.makeText(this, "No hay dispositivos vinculados", Toast.LENGTH_SHORT).show();
         }
         adapter.notifyDataSetChanged();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (btAdapter.isDiscovering()) {
+            btAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(mReceiver);
     }
 }
